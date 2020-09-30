a5dd7d58647d8057a6288a635496516945f69c9f
Richie1136 <Richiemajor1@gmail.com>
2020-09-30 15:16:01 -0400

    updated CSS

--- code.js
+++ code.js
@@ -1,34 +1,42 @@
 let setDiscCount = 0; //adding to for loop
 let connectFourBoardModel = [[null, null, null, null, null, null, null],
-                             [null, null, null, null, null, null, null],
-                             [null, null, null, null, null, null, null],
-                             [null, null, null, null, null, null, null],
-                             [null, null, null, null, null, null, null],
-                             [null, null, null, null, null, null, null]]
+[null, null, null, null, null, null, null],
+[null, null, null, null, null, null, null],
+[null, null, null, null, null, null, null],
+[null, null, null, null, null, null, null],
+[null, null, null, null, null, null, null]]
+
+
+
+let winningCombinations = [
+    [0, 0]
+]
+{ 0 }
+
 let gameState = 'red'
-let droppedDiscIntoColumn = function(columnElement) {
+let droppedDiscIntoColumn = function (columnElement) {
     //this function will be checking for if there a column is completely full,
     // if the column is full, make sure that nothing is able to be placed in the column.
 }
-let createColumnEventListener = function(){
+let createColumnEventListener = function () {
     //add a queryselector
-    document.querySelector('#col1').addEventListener('click',columnClickHandler)
-    document.querySelector('#col2').addEventListener('click',columnClickHandler)
-    document.querySelector('#col3').addEventListener('click',columnClickHandler)
-    document.querySelector('#col4').addEventListener('click',columnClickHandler)
-    document.querySelector('#col5').addEventListener('click',columnClickHandler)
-    document.querySelector('#col6').addEventListener('click',columnClickHandler)
-    document.querySelector('#col7').addEventListener('click',columnClickHandler)
+    document.querySelector('#col1').addEventListener('click', columnClickHandler)
+    document.querySelector('#col2').addEventListener('click', columnClickHandler)
+    document.querySelector('#col3').addEventListener('click', columnClickHandler)
+    document.querySelector('#col4').addEventListener('click', columnClickHandler)
+    document.querySelector('#col5').addEventListener('click', columnClickHandler)
+    document.querySelector('#col6').addEventListener('click', columnClickHandler)
+    document.querySelector('#col7').addEventListener('click', columnClickHandler)
     // document.querySelector('col2').addEventListener(click,columnClickHandler
 }
- 
-let columnClickHandler = function(evt){
+
+let columnClickHandler = function (evt) {
     //whenever the mouse hovers over this column, it'll click the item
     let clicked = evt.currentTarget
     console.log(clicked)
 }
 
-let switchPlayers = function(){
+let switchPlayers = function () {
     //whenever red makes their move, it will become yellow turn, gameState equals yellow
     //whenever yellow makes their move, it will become red turn, gameState equals red
 }
