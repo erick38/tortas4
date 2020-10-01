e7c7db2d521b0cc1f1d52202237167364193d7a5
Richie1136 <Richiemajor1@gmail.com>
2020-10-01 16:24:15 -0400

    disc colors showing up and fixed column to limit to 6 allowed

--- code.js
+++ code.js
@@ -1,15 +1,11 @@
 let setDiscCount = 0; //adding to for loop
 
-// let red = document.querySelector('.red')
 
-// let yellow = document.querySelector('.yellow')
 let playerDisc;
 let board = document.querySelector("#board");
-// player1[]
-// player2[]
-// let playerRed;
-// let playerYellow; 
+
 let player = "playerYellow";
+let gameState = "red";
 let boardModel = [
   [null, null, null, null, null, null, null],
   [null, null, null, null, null, null, null],
@@ -18,143 +14,97 @@ let boardModel = [
   [null, null, null, null, null, null, null],
   [null, null, null, null, null, null, null],
 ];
-let gameState = "red";
-let droppedDiscIntoColumn = function (columnElement) {
-  //this function will be checking for if there a column is completely full,
-  // if the column is full, make sure that nothing is able to be placed in the column.
-  let full = 0;
+
+let switchPlayers = function () {
+  playerDisc = document.createElement("div");
+  if (player === "playerYellow") {
+    player = "playerRed";
+
+    console.log("1");
+
+  } else {
+    player = "playerYellow";
+    console.log("2");
+  }
+
 };
+
+let columnClickHandler = function (evt) {
+
+  let clicked = evt.currentTarget;
+
+  let col = Number(clicked.id.slice(-1));
+
+  for (let row = boardModel.length - 1; row >= 0; row--) {
+    if (boardModel[row][col] === null) {
+      boardModel[row][col] = player;
+      break;
+    }
+  }
+
+
+  playerDisc = document.createElement("div");
+  playerDisc.className = "piece";
+
+  if (player === "playerYellow" && clicked.childElementCount < 6) {
+    playerDisc.classList.add("piece", "yellow");
+    clicked.appendChild(playerDisc);
+  }
+  if (player === "playerRed" && clicked.childElementCount < 6) {
+    playerDisc.classList.add("piece", "red");
+    clicked.appendChild(playerDisc);
+  }
+  switchPlayers();
+
+};
+
+//whenever red makes their move, it will become yellow turn, gameState equals yellow
+//whenever yellow makes their move, it will become red turn, gameState equals red
+
 let createColumnEventListener = function () {
-  //add a queryselector
+
+  document.querySelector("#col0").addEventListener("click", columnClickHandler);
   document.querySelector("#col1").addEventListener("click", columnClickHandler);
   document.querySelector("#col2").addEventListener("click", columnClickHandler);
   document.querySelector("#col3").addEventListener("click", columnClickHandler);
   document.querySelector("#col4").addEventListener("click", columnClickHandler);
   document.querySelector("#col5").addEventListener("click", columnClickHandler);
   document.querySelector("#col6").addEventListener("click", columnClickHandler);
-  document.querySelector("#col7").addEventListener("click", columnClickHandler);
-  // document.querySelector('col2').addEventListener(click,columnClickHandler
 };
-
-
-let columnClickHandler = function (evt) {
-  //whenever the mouse hovers over this column, it'll click the item
-  let clicked = evt.currentTarget;
-  // console.log(clicked)
-//   switchPlayers();
-  playerDisc = document.createElement("div");
-    playerDisc.className= 'piece'
-    // playerDisc.className = 'yellow'
-    clicked.appendChild(playerDisc)
-    switchPlayers();
-    // if (player === 'playerYellow') {
-    //     playerDisc.classList.add("piece", "yellow");
-    //     clicked.appendChild(playerDisc);
-    // } else {
-    //     playerDisc.classList.add('piece','red')
-    //     clicked.appendChild(playerDisc);
-    // }
-};
-let switchPlayers = function () {
-  playerDisc = document.createElement("div");
-  if (player === "playerYellow") {
-    // playerDisc.classList.add("piece", "yellow");
-    player = "playerRed";
-
-    console.log("1");
-    
-  } else {
-    // playerDisc.classList.add("piece", "red");
-    player = "playerYellow";
-    console.log("2");
-    
-  }
-
-  //whenever red makes their move, it will become yellow turn, gameState equals yellow
-  //whenever yellow makes their move, it will become red turn, gameState equals red
-};
-
 createColumnEventListener();
-let col;
 
-// for (col = 0; i < boardModel.length; col++){
-//     const newMap = boardModel[col]
-//     //
-//}
-/*
-First Step
-Creating the game
-1.) Board View
-2.) Add Click Handlers
+  // First Step
+  // Creating the game
+  // 1.) Board View
+  // 2.) Add Click Handlers
 
 
-Second Step
+  // Second Step
 
 
-Psuedocode if statement
-I'll need a game state for this game.
-if gameState is set 'red', red may drop their piece in a column
-set the game state to yellow
-If gamestate is yellow, yellow may drop their piece in a column */
+  // Psuedocode if statement
+  // I'll need a game state for this game.
+  // if gameState is set 'red', red may drop their piece in a column
+  // set the game state to yellow
+  // If gamestate is yellow, yellow may drop their piece in a column */
 
-//Lines Below is old code 
-// All Code Here will be rechecked
-// [let setDiscCount = 0; //adding to for loop
-// let connectFourBoardModel = [[null, null, null, null, null, null, null],
-// [null, null, null, null, null, null, null],
-// [null, null, null, null, null, null, null],
-// [null, null, null, null, null, null, null],
-// [null, null, null, null, null, null, null],
-// [null, null, null, null, null, null, null]]
+  //Lines Below is old code 
+  // All Code Here will be rechecked
 
-
-
-// // let winningCombinations = [
-// //     [0, 0]
-// // ]
-// // { 0 }
-
-// let gameState = 'red'
-// let droppedDiscIntoColumn = function (columnElement) {
-//     //this function will be checking for if there a column is completely full,
-//     // if the column is full, make sure that nothing is able to be placed in the column.
-// }
-// let createColumnEventListener = function () {
-//     //add a queryselector
-//     document.querySelector('#col1').addEventListener('click', columnClickHandler)
-//     document.querySelector('#col2').addEventListener('click', columnClickHandler)
-//     document.querySelector('#col3').addEventListener('click', columnClickHandler)
-//     document.querySelector('#col4').addEventListener('click', columnClickHandler)
-//     document.querySelector('#col5').addEventListener('click', columnClickHandler)
-//     document.querySelector('#col6').addEventListener('click', columnClickHandler)
-//     document.querySelector('#col7').addEventListener('click', columnClickHandler)
-//     // document.querySelector('col2').addEventListener(click,columnClickHandler
-// }
-
-// let columnClickHandler = function (evt) {
-//     //whenever the mouse hovers over this column, it'll click the item
-//     let clicked = evt.currentTarget
-//     console.log(clicked)
-// }
-
-// let switchPlayers = function () {
-//     //whenever red makes their move, it will become yellow turn, gameState equals yellow
-//     //whenever yellow makes their move, it will become red turn, gameState equals red
-// }
-// createColumnEventListener()
-// /*
-// First Step
-// Creating the game
-// 1.) Board View
-// 2.) Add Click Handlers
+  // createColumnEventListener()
+  // /*
+  // First Step
+  // Creating the game
+  // 1.) Board View
+  // 2.) Add Click Handlers
 
 
-// Second Step
+  // Second Step
 
-
-// Psuedocode if statement
-// I'll need a game state for this game.
-// if gameState is set 'red', red may drop their piece in a column
-// set the game state to yellow
-// If gamestate is yellow, yellow may drop their piece in a column */
-// //----------------------------------------------------------------]
+  // /*
+  // Psuedocode if statement
+  // I'll need a game state for this game.
+  // if gameState is set 'red', red may drop their piece in a column
+  // set the game state to yellow
+  // If gamestate is yellow, yellow may drop their piece in a column */
+  //
